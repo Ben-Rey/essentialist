0120565d10ff753adee7bb86c5253f1c9ce335da
Ben <br@massa.net>
2023-03-19 14:45:18 +0100

    should return Fizz on multiple of 3 refacto

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,12 +1,4 @@
 export function fizzbuzz(n: number): string {
-    if (n === 3) {
-        return "Fizz";
-    }
-    if (n === 6) {
-        return "Fizz";
-    }
-    if (n === 12) {
-        return "Fizz";
-    }
+    if (n % 3 === 0) return "Fizz";
     return n.toString();
 }
