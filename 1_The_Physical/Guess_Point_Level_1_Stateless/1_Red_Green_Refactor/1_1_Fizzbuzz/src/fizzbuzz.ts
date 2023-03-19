1c908e05f11dd56961cf88ad6819f11190168743
Ben <br@massa.net>
2023-03-19 14:47:21 +0100

    should return Fizz on multiple of 5 refacto

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,7 +1,5 @@
 export function fizzbuzz(n: number): string {
     if (n % 3 === 0) return "Fizz";
-    if (n === 5) return "Buzz";
-    if (n === 10) return "Buzz";
-    if (n === 20) return "Buzz";
+    if (n % 5 === 0) return "Buzz";
     return n.toString();
 }
