a4e0af1d540a17f853058b797bfa5b59cec10928
Ben <br@massa.net>
2023-03-20 21:51:56 +0100

    [test] should return true if text is 1 char

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,8 +1,13 @@
 import { isPalindrome } from './index'
 
 describe('palindrome checker', () => {
-    it('sould return false for ""', () => {
+    it('should return false for ""', () => {
         expect(isPalindrome('')).toBe(false)
     })
+
+    it('should return true for "a"', () => {
+        expect(isPalindrome('a')).toBe(true)
+    })
+
 })
 
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,3 +1,4 @@
 export function isPalindrome(text: string): any {
     if (text.length === 0) return false
+    return true
 }
