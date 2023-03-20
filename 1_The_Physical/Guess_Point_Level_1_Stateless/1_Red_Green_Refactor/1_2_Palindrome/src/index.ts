7c7e64b47ce2b404564b4a20f78c15e47636d65d
Ben <br@massa.net>
2023-03-20 22:09:37 +0100

    [clean] refacto code

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,6 +1,4 @@
 export function isPalindrome(text: string): any {
-    if (text.length === 0) return false
-    if (text.length === 2 && text[0] !== text[1]) return false
-    if (text.split('').reverse().join('') !== text) return false
-    return true
+    if (!text) return false
+    return text.split('').reverse().join('') === text
 }
